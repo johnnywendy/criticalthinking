b076d543c81238cb0d1f9954df41a694d293494e
hmcdona1 <gmail@reecemcdonald.com>
2014-10-02 10:49:50 -0500

    Actual First Commit

---
+++ Assets/BAH Assets/Characters/Scripts/IKLookAt.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]  
+
+public class IKLookAt : MonoBehaviour 
+{	
+	protected Animator avatar;
+	protected IKActivator activator;
+	public float lerpWeight;
+	
+	public bool ikActive = false;
+	public Transform lookAtObj = null;
+
+	public float lookAtWeight = 0.0f;
+
+	void Start() 
+	{
+		avatar = GetComponent<Animator>();
+		activator = GetComponent<IKActivator>();
+	}
+
+	public void Enable()
+	{
+		avatar = avatar ?? GetComponent<Animator>();
+		this.enabled = true;
+		ikActive = true;
+	}
+
+	public void Disable()
+	{
+		this.enabled = false;
+		ikActive = false;
+	}
+
+	void OnAnimatorIK(int layerIndex)
+	{	
+		if(avatar)
+		{
+			//if IK is active, continuously sets the look at weight based on IKActivator lerpedLookWeight
+			if(ikActive)
+			{
+				avatar.SetLookAtWeight(lerpWeight,0.15f, 0.75f, 1.0f, 0.5f);
+
+				if(lookAtObj != null)
+				{
+					avatar.SetLookAtPosition(lookAtObj.position);
+				}	
+			}
+
+			else
+			{	
+				avatar.SetLookAtWeight(1.0f);
+			}
+		}
+	}
+
+	void Update () 
+	{
+		if(avatar)
+		{
+			if(!ikActive)
+			{
+				if(lookAtObj != null)
+				{
+					lookAtObj.position = avatar.bodyPosition + avatar.bodyRotation * new Vector3(0,0.5f,1);
+				}				
+			}
+		}		
+	}   		  
+}
